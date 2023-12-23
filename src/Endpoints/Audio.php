430d82de5cbaee3c1696bd1181c64bd2e51986d3
Kepsondiaz <kherabadiaby328@gmail.com>
2023-12-22 20:42:54 -0500

    improve: improve function on audio

--- src/Endpoints/Audio.php
+++ src/Endpoints/Audio.php
@@ -3,6 +3,7 @@
 namespace Kepson\SimplestOpeniaSdk\Endpoints;
 use GuzzleHttp\Client;
 use Exception;
+use GuzzleHttp\Exception\BadResponseException;
 use Kepson\SimplestOpeniaSdk\Enums\Audios\Speech\Model;
 use Kepson\SimplestOpeniaSdk\Enums\Audios\Speech\Voice;
 use Kepson\SimplestOpeniaSdk\Enums\Audios\Transcriptions\ResponseFormat;
@@ -19,59 +20,77 @@ class Audio {
 
     public function createSpeech(Model $model, string $input, Voice $voice, float $speed = 1.0)
     {
-        $response = $this->httpClient->post( 'https://api.openai.com/v1/audio/speech', [
-            'headers' => [
-                'Authorization' => 'Bearer' . config('services.openia.YOUR_OPENIA_API_KEY'),
-                'Content-Type' => 'application/json'
-            ],
-            'json' => [
-                'model' => $model, 
-                'input' => $input,
-                'voice' => $voice,
-                'speed' => $speed
-            ],
-        ]);
-
-        return $response; 
+        try {
+            $response = $this->httpClient->post( 'https://api.openai.com/v1/audio/speech', [
+                'headers' => [
+                    'Authorization' => 'Bearer' . config('services.openai.api_key'),
+                    'Content-Type' => 'application/json'
+                ],
+                'json' => [
+                    'model' => $model, 
+                    'input' => $input,
+                    'voice' => $voice,
+                    'speed' => $speed
+                ],
+            ]);
+    
+            return $response;
+            
+        } catch (BadResponseException $e) {
+            return $e;
+        } 
     }
 
 
     public function createTranscription(string $file_path, $model, $langage = "eng", $prompt = '', ResponseFormat $response_format = ResponseFormat::JSON, int $temperature)
     {
-        $response = $this->httpClient->post( 'https://api.openai.com/v1/audio/transcriptions', [
-            'headers' => [
-                'Authorization' => 'Bearer' . config('services.openia.YOUR_OPENIA_API_KEY'),
-                'Content-Type' => 'multipart/form-data'
-            ],
-            'json' => [
-                'model' => $model, 
-                'file_path' => $file_path,
-                'langage' => $langage,
-                'prompt' => $prompt,
-                'response_format' => $response_format,
-                'temperature' => $temperature
-            ],
-        ]);
+        try {
 
-        return $response; 
+            $response = $this->httpClient->post( 'https://api.openai.com/v1/audio/transcriptions', [
+                'headers' => [
+                    'Authorization' => 'Bearer' . config('services.openai.api_key'),
+                    'Content-Type' => 'multipart/form-data'
+                ],
+                'json' => [
+                    'model' => $model, 
+                    'file_path' => $file_path,
+                    'langage' => $langage,
+                    'prompt' => $prompt,
+                    'response_format' => $response_format,
+                    'temperature' => $temperature
+                ],
+            ]);
+
+            return $response;
+            
+        } catch (BadResponseException $e) {
+            return $e; 
+        }
+ 
     }
 
     public function createTranslation(string $file_path, $model, $prompt = '', ResponseFormat $response_format = ResponseFormat::JSON, int $temperature)
     {
-        $response = $this->httpClient->post( 'https://api.openai.com/v1/audio/translations', [
-            'headers' => [
-                'Authorization' => 'Bearer' . config('services.openia.YOUR_OPENIA_API_KEY'),
-                'Content-Type' => 'multipart/form-data'
-            ],
-            'json' => [
-                'model' => $model, 
-                'file_path' => $file_path,
-                'prompt' => $prompt,
-                'response_format' => $response_format,
-                'temperature' => $temperature
-            ],
-        ]);
+        try {
 
-        return $response;
+            $response = $this->httpClient->post( 'https://api.openai.com/v1/audio/translations', [
+                'headers' => [
+                    'Authorization' => 'Bearer' . config('services.openai.api_key'),
+                    'Content-Type' => 'multipart/form-data'
+                ],
+                'json' => [
+                    'model' => $model, 
+                    'file_path' => $file_path,
+                    'prompt' => $prompt,
+                    'response_format' => $response_format,
+                    'temperature' => $temperature
+                ],
+            ]);
+    
+            return $response;
+            
+        } catch (BadResponseException $e) {
+            return $e; 
+        }
     }
 }
