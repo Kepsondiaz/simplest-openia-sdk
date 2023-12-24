b15f6499632fa2e4b0f9482f230381742d17c254
Kepsondiaz <kherabadiaby328@gmail.com>
2023-12-24 11:59:11 -0500

    add imageCompletion function

--- src/Endpoints/Chat.php
+++ src/Endpoints/Chat.php
@@ -44,4 +44,45 @@ class Chat
          
     }
 
+    public function imagesCompletions(string $text, $image_url, $role = "user", $model, int $frequency_penalty = 0, object $response_format, $max_token = 300)
+    {
+        try {
+
+            $response = $this->httpClient->post( 'https://api.openai.com/v1/chat/completions', [
+                'headers' => [
+                    'Authorization' => 'Bearer' . config('services.openai.api_key'),
+                    'Content-Type' => 'application/json'
+                ],
+                'json' => [
+                    'model' => $model, 
+                    'messages' => [
+                        "role" => $role,
+                        "content" => [
+                            [
+                              "type" => "text",
+                              "text" => $text
+                            ],
+                            [
+                              "type" => "image_url",
+                              "image_url" => [
+                                "url" => $image_url
+                                ]
+                            ]
+                          ]
+                    ],
+                    "frequency_penalty" => $frequency_penalty,
+                    "response_format" => $response_format,
+                    "max_tokens" => $max_token
+                ],
+            ]);
+
+            return $response;
+        }
+        catch(BadResponseException $e)
+        {
+            return $e;
+        }
+         
+    }
+
 }
